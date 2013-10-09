994ec25dd1d22586a688d9105cc110172003ed7d
Eric Hagman <eric@hax.(none)>
2013-10-09 00:42:51 -0400

    fix

--- bar/views.py
+++ bar/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from tools.database import Database
+import re
+import hashlib
+from sqlalchemy.exc import SQLAlchemyError
+from django.views.decorators.csrf import csrf_exempt
+from dateutil.parser import *
+from django.http import HttpResponse
+import json
+
+@csrf_exempt
+def list(request):
+    return HttpResponse(json.loads([{"name": "Kelly's Tap House", "address": "Heaven"}]), content_type="application/json")
--- bartalk/urls.py
+++ bartalk/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
                        url(r'^api/drinkers/register', 'drinker.views.register', name='register'),
-                       url(r'^api/drinkers/login', 'drinker.views.login', name='login'), )
+                       url(r'^api/drinkers/login', 'drinker.views.login', name='login'),
+                       url(r'^api/bars/list', 'bar.views.list', name='list_bars'),)
